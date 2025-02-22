617c7513c29f06467c35abbf2b6cbf42d873c008
Tammura <ahmedtamer552001@gmail.com>
2025-02-22 19:55:31 +0100

    Add pagination

--- django_lib/library/views.py
+++ django_lib/library/views.py
@@ -2,13 +2,18 @@ from rest_framework import generics
 from .models import Libro
 from .serializers import LibroSerializer
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 
 class LibroListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Libro.objects.all().prefetch_related("autori").order_by("anno_edizione")
+    queryset = Libro.objects.all().order_by("anno_edizione")
     serializer_class = LibroSerializer
 
 
 def books_list(request):
-    books = Libro.objects.all().prefetch_related("autori").order_by("anno_edizione")
-    return render(request, "home.html", {"books": books})
+    page_number = request.GET.get("page")
+    books = Libro.objects.all().order_by("anno_edizione")
+    paginator = Paginator(books, 10)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "home.html", {"page_obj": page_obj})
